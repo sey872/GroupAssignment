3c1793457f2ea72be13d2b3a59e81bd13cb25c1d
Lynzoid <jacksoncollins1994@gmail.com>
2016-06-03 18:49:18 +1000

    Fixed wasted functions

---
+++ README.txt
@@ -0,0 +1,9 @@
+Group Leader: Jackson Collins
+
+Students
+Name:Jackson Collins
+Email:jrc772@uowmail.edu.au
+
+Name:Scott Young
+Email:sey872@uowmail.edu.au
+
--- app/src/main/java/com/example/scott/groupassignment/MainActivity.java
+++ app/src/main/java/com/example/scott/groupassignment/MainActivity.java
@@ -29,18 +29,14 @@ public class MainActivity extends FragmentActivity implements AdapterView.OnItem
 
     private Bundle bundle;
     private GoogleMap map;
-    private ArrayList<LatLng> markerPoints;
-    private TextView tvDistanceDuration;
+    //private ArrayList<LatLng> markerPoints;
+    //private TextView tvDistanceDuration;
     protected static boolean isMainShown = false;
-    private LocationManager locationManager;
-    private LocationListener locationListener;
+    //private LocationManager locationManager;
+    //private LocationListener locationListener;
 
     private List<storeList> store;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+
     private double latitude;
     private double longitude;
 
@@ -124,8 +120,6 @@ public class MainActivity extends FragmentActivity implements AdapterView.OnItem
                 startActivity(i);
             }
         });
-
-        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 
 
         /*// Initializing
@@ -440,44 +434,4 @@ public class MainActivity extends FragmentActivity implements AdapterView.OnItem
             super.onBackPressed();
         }
     }
-/*
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.scott.groupassignment/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.scott.groupassignment/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }*/
 }
